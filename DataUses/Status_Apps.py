202c2d776c76244590fdc580b0fbf621cd93f61b
Saket Vats <saketvatslpu@gmail.com>
2020-09-09 17:55:54 +0530

    Changes in Status App

--- DataUses/Status_Apps.py
+++ DataUses/Status_Apps.py
@@ -10,7 +10,6 @@ validIds = ['lDashBatteryQuantity', 'lDashChargeStatus', 'lulCurrentDataRate', '
 if jio_req.ifResponseOk():
     print(jio_req.getSelectedData(validIds))
 
-
 from PyQt5 import QtWidgets, QtGui, QtCore
 class PyQtApp(QtWidgets.QWidget):
    
@@ -18,10 +17,27 @@ class PyQtApp(QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self, parent)
         self.setWindowTitle("PyQt Application")
         self.setWindowIcon(QtGui.QIcon("Your/image/file.png"))
+        self.setMinimumWidth(resolution.width() / 3)
+        self.setMinimumHeight(resolution.height() / 1.5)
+        self.setStyleSheet('''QWidget {background-color:
+                           rgba(0,41,59,255);}
+                           QScrollBar:horizontal {
+                           width: 1px; height: 1px;
+                           background-color: rgba(0,41,59,255);}  
+                           QScrollBar:vertical {width: 1px;
+                           height: 1px;
+                           background-color: rgba(0,41,59,255);}''')
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
+    desktop = QtWidgets.QApplication.desktop()
+    resolution = desktop.availableGeometry()
     myapp = PyQtApp()
+    myapp.setWindowOpacity(0.95)
     myapp.show()
+    myapp.move(resolution.center() - myapp.rect().center())
     sys.exit(app.exec_())
+else:
+    desktop = QtWidgets.QApplication.desktop()
+    resolution = desktop.availableGeometry()
